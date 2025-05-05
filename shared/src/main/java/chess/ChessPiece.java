b1f143e114cfa2c7035a9ea274042dadab01a4ab
Mona Ueno <monaueno@Monas-MacBook-Air-3.local>
2025-05-05 11:32:30 -0600

    Passed off bishopMoveUntilEdge

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -58,7 +58,31 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
+        for (int i = 0; i < directions.length; i++) {
+            int[] dir = directions[i];
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            while (true) {
+                row += dir[0];
+                col += dir[1];
+                if (row < 1 || row > 8 || col < 1 || col > 8) {
+                    break;
+                }
+                ChessPosition newPos = new ChessPosition(row, col);
+                ChessPiece targetPiece = board.getPiece(newPos);
+                if (targetPiece == null) {
+                    moves.add(new ChessMove(myPosition, newPos, null));
+                } else {
+                    if (targetPiece.getTeamColor() != this.getTeamColor()) {
+                        moves.add(new ChessMove(myPosition, newPos, null));
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
     }
 }
