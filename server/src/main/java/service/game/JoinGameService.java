41e9eb5545ac64abd8b53e9f7215fca710f23349
Mona Ueno <monaueno@Monas-MacBook-Air-3.local>
2025-06-06 09:25:06 -0600

    still debugging same user and same color

--- server/src/main/java/service/game/JoinGameService.java
+++ server/src/main/java/service/game/JoinGameService.java
@@ -30,6 +30,10 @@ public class JoinGameService {
         }
 
         String username = auth.username();
+        System.out.println("Comparing: game.whiteUsername=[" + game.whiteUsername() + "] to auth.username=[" + username + "]");
+        System.out.println("Joining as: " + username);
+        System.out.println("whiteUsername: " + game.whiteUsername());
+        System.out.println("Requested color: " + request.playerColor());
         System.out.println("🎯 request.playerColor() = [" + request.playerColor() + "]");
 
         if (request.playerColor() == null) {
@@ -46,10 +50,12 @@ public class JoinGameService {
                 throw new DataAccessException("Invalid team color: " + request.playerColor());
             }
 
-            if (color == ChessGame.TeamColor.WHITE && game.whiteUsername() != null) {
+            // System.out.println("Comparing: game.whiteUsername=[" + game.whiteUsername() + "] to auth.username=[" + username + "]");
+
+            if (color == ChessGame.TeamColor.WHITE && game.whiteUsername() != null && !game.whiteUsername().equals(username)) {
                 throw new DataAccessException("already taken");
             }
-            if (color == ChessGame.TeamColor.BLACK && game.blackUsername() != null) {
+            if (color == ChessGame.TeamColor.BLACK && game.blackUsername() != null && !game.blackUsername().equals(username)) {
                 throw new DataAccessException("already taken");
             }
 
