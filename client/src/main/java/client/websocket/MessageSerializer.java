c591c50b89f27adcd28b2a791152fe890c193147
Mona Ueno <monaueno@Monas-MacBook-Air-3.local>
2025-06-14 15:24:55 -0600

    last thing

--- client/src/main/java/client/websocket/MessageSerializer.java
+++ client/src/main/java/client/websocket/MessageSerializer.java
@@ -7,18 +7,18 @@ import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 public class MessageSerializer {
-    private static final Gson gson = new Gson();
+    private static final Gson GSON = new Gson();
 
     public static ServerMessage deserializeServerMessage(String json) {
-        ServerMessage baseMessage = gson.fromJson(json, ServerMessage.class);
+        ServerMessage baseMessage = GSON.fromJson(json, ServerMessage.class);
 
         switch (baseMessage.getServerMessageType()) {
             case LOAD_GAME:
-                return gson.fromJson(json, LoadGameMessage.class);
+                return GSON.fromJson(json, LoadGameMessage.class);
             case NOTIFICATION:
-                return gson.fromJson(json, NotificationMessage.class);
+                return GSON.fromJson(json, NotificationMessage.class);
             case ERROR:
-                return gson.fromJson(json, ErrorMessage.class);
+                return GSON.fromJson(json, ErrorMessage.class);
             default:
                 throw new IllegalArgumentException("Unknown server message type");
         }
